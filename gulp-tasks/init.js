dfcb050a201e9e2809d1280f3e2ea1bf847d4458
Jacob Bearce <jacob@bearce.me>
2017-08-08 14:55:59 -0500

    Basics for a better init task

--- gulp-tasks/init.js
+++ gulp-tasks/init.js
@@ -2,80 +2,95 @@
 
 // Scripts written by YOURNAME @ YOURCOMPANY
 
-module.exports = (gulp, plugins) => {
-    // WIP: init task, initializes the project
-    return () => {
-        return new Promise ((resolve) => {
-            // remove any specified optional modules
-            gulp.src(global.settings.paths.src + "/**/*")
-                // check if a file is a binary
-                .pipe(plugins.is_binary())
-                // skip file if it's a binary
-                .pipe(plugins.through.obj((file, enc, next) => {
-                    if (file.isBinary()) {
-                        next();
-                        return;
+module.exports = {
+    init(gulp, plugins, on_error) {
+        let project_data = {};
+
+        // gather project data
+        const get_project_data = (callback) => {
+            return gulp.src("./gulpfile.js")
+                // prevent breaking on error
+                .pipe(plugins.plumber({errorHandler: on_error}))
+                // prompt for project data
+                .pipe(plugins.prompt.prompt([
+                    {
+                        name:    "name",
+                        message: "Project Name",
+                        type:    "input",
+                    },
+                    {
+                        name:    "homepage",
+                        message: "Project URL",
+                        type:    "input",
+                    },
+                    {
+                        name: "repository",
+                        message: "Project Repository",
+                        type:    "input",
+                    },
+                    {
+                        name: "author_name",
+                        message: "Author Name",
+                        type:    "input",
+                    },
+                    {
+                        name: "author_company",
+                        message: "Author Company",
+                        type:    "input",
+                    },
+                    {
+                        name: "author_email",
+                        message: "Author Email",
+                        type:    "input",
+                    },
+                    {
+                        name: "author_url",
+                        message: "Author URL",
+                        type:    "input",
+                    },
+                ], (res) => {
+                    // store the project data
+                    project_data = res;
+                })).on("end", () => {
+                    // return the callback
+                    if (typeof callback === "function") {
+                        return callback();
                     }
+                });
+        };
 
-                    // go to next file
-                    next(null, file);
+        // write project data
+        const write_project_data = (callback) => {
+            return gulp.src("./package.json")
+                // replace variables with project data
+                .pipe(plugins.file_include({
+                    prefix:   "@@",
+                    basepath: "@file",
+                    context: {
+                        name:           project_data.name,
+                        homepage:       project_data.homepage,
+                        repository:     project_data.repository,
+                        author_name:    project_data.author_name,
+                        author_company: project_data.author_company,
+                        author_email:   project_data.author_email,
+                        author_url:     project_data.author_url,
+                    }
                 }))
-                // remove login code if --remove login is passed
-                .pipe(plugins.remove_code({login_html: plugins.argv.remove && plugins.argv.remove.includes("login") ? true : false, commentStart: "<!--", commentEnd: "-->"}))
-                .pipe(plugins.remove_code({login_css_js_php: plugins.argv.remove && plugins.argv.remove.includes("login") ? true : false, commentStart: "/*", commentEnd: "*/"}))
-                // remove resource code if --remove tribe is passed
-                .pipe(plugins.remove_code({resources_html: plugins.argv.remove && plugins.argv.remove.includes("resources") ? true : false, commentStart: "<!--", commentEnd: "-->"}))
-                .pipe(plugins.remove_code({resources_css_js_php: plugins.argv.remove && plugins.argv.remove.includes("resources") ? true : false, commentStart: "/*", commentEnd: "*/"}))
-                // remove tribe code if --remove tribe is passed
-                .pipe(plugins.remove_code({tribe_html: plugins.argv.remove && plugins.argv.remove.includes("tribe") ? true : false, commentStart: "<!--", commentEnd: "-->"}))
-                .pipe(plugins.remove_code({tribe_css_js_php: plugins.argv.remove && plugins.argv.remove.includes("tribe") ? true : false, commentStart: "/*", commentEnd: "*/"}))
-                // output to source directory
-                .pipe(gulp.dest(global.settings.paths.src))
-                // resolve the promise
-                .on("end", () => {
-                    // resolve the promise
-                    resolve();
+                // write the file
+                .pipe(gulp.dest("./")).on("end", () => {
+                    // return the callback
+                    if (typeof callback === "function") {
+                        return callback();
+                    }
                 });
-        }).then(() => {
-            return new Promise ((resolve) => {
-                // remove any empty files
-                plugins.glob(global.settings.paths.src + "/**/*", (err, files) => {
-                    files.forEach((file) => {
-                        if (plugins.fs.statSync(file).size <= 1) {
-                            plugins.fs.unlinkSync(file);
-                            console.log("\x1b[32m✔\x1b[0m deleted: " + plugins.path.relative(process.cwd(), file));
-                        }
-                    });
+        };
 
-                    // resolve the promise
-                    resolve();
-                });
-            }).then(() => {
-                // remove any empty folders
-                return new Promise ((resolve) => {
-                    plugins.delete_empty(global.settings.paths.src, () => {
-                        // resolve the promise
-                        resolve();
-                    });
-                }).then(() => {
-                    // remove any remaining comments
-                    gulp.src(global.settings.paths.src + "/**/*")
-                        // check if a file is a binary
-                        .pipe(plugins.is_binary())
-                        // skip file if it's a binary
-                        .pipe(plugins.through.obj((file, enc, next) => {
-                            if (file.isBinary()) {
-                                next();
-                                return;
-                            }
-
-                            // go to next file
-                            next(null, file);
-                        }))
-                        .pipe(plugins.replace(/((?:\/\*|<!--)(?:end)?[rR]emoveIf\([^)]+\)(?:\*\/|-->))/g, ""))
-                        .pipe(gulp.dest(global.settings.paths.src));
+        return new Promise ((resolve) => {
+            get_project_data(() => {
+                write_project_data(() => {
+                    return resolve();
                 });
             });
         });
-    };
+    }
 };
