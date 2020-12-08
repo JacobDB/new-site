935bda2ffe0356222193535962f9ce45b9aef98b
Jacob Bearce <jacob@weblinxinc.com>
2020-12-08 11:07:18 -0600

    Disallow editing theme/plugin files and adding additional CSS

--- src/functions/theme-features.php
+++ src/functions/theme-features.php
@@ -16,3 +16,21 @@ add_theme_support("title-tag");
 add_theme_support("automatic-feed-links");
 
 add_theme_support("post-thumbnails");
+
+/**
+ * Disable "Additional CSS" in the Customizer screen
+ *
+ * @param object $wp_customize
+ * @return void
+ */
+function __gulp_init_namespace___disable_additional_css(object $wp_customize): void {
+    $wp_customize->remove_control("custom_css");
+}
+add_action("customize_register", "__gulp_init_namespace___disable_additional_css");
+
+/**
+ * Disallow file editing
+ */
+if (! defined("DISALLOW_FILE_EDIT")) {
+    define("DISALLOW_FILE_EDIT", true);
+}
