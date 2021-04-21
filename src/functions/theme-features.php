1edef149bc5ada29e7078828e30ddd4112d78781
Jacob Bearce <jacob@weblinxinc.com>
2021-04-21 09:57:43 -0500

    Add button format to TinyMCE

--- src/functions/theme-features.php
+++ src/functions/theme-features.php
@@ -29,6 +29,38 @@ function __gulp_init_namespace___disable_additional_css(WP_Customize_Manager $wp
 add_action("customize_register", "__gulp_init_namespace___disable_additional_css");
 
 /**
+ * Enable "Formats" button in TinyMCE
+ *
+ * @param array $buttons
+ * @return array
+ */
+function __gulp_init_namespace___tinymce_enable_format_button(array $buttons): array {
+    array_unshift($buttons, "styleselect");
+
+    return $buttons;
+}
+add_filter("mce_buttons_2", "__gulp_init_namespace___tinymce_enable_format_button", 10, 1);
+
+/**
+ * Add 'Button' format to TinyMCE
+ *
+ * @param array $init_array
+ * @return array
+ */
+function __gulp_init_namespace___tinymce_button_format(array $init_array): array {
+    $init_array["style_formats"] = wp_json_encode([
+        [
+            "title"    => esc_html__("Button", "__gulp_init_namespace__"),
+            "classes"  => "button",
+            "selector" => "a",
+        ],
+    ]);
+
+    return $init_array;
+}
+add_filter("tiny_mce_before_init", "__gulp_init_namespace___tinymce_button_format", 10, 1);
+
+/**
  * Disallow file editing
  */
 if (! defined("DISALLOW_FILE_EDIT")) {
