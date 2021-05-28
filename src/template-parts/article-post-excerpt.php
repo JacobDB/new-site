f92e3ff555e7ed326398420dca6f9103fbed9988
Jacob Bearce <jacob@weblinxinc.com>
2021-05-28 15:17:28 -0500

    Adjust post-excerpt article variant to match global convention

--- src/template-parts/article-post-excerpt.php
+++ src/template-parts/article-post-excerpt.php
@@ -3,13 +3,13 @@ $post      = isset($this->vars["post"]) ? $this->vars["post"] : false;
 $class     = isset($this->vars["class"]) ? " {$this->vars["class"]}" : "";
 $light     = isset($this->vars["light"]) ? $this->vars["light"] : false;
 $permalink = isset($this->vars["permalink"]) ? $this->vars["permalink"] : ($post ? get_the_permalink($post->ID) : false);
-$title     = isset($this->vars["title"]) ? $this->vars["title"] : ($post ? $post->post_title : false);
+$title     = isset($this->vars["title"]) ? $this->vars["title"] : ($post ? apply_filters("the_title", $post->post_title, $post->ID) : "");
 $meta      = isset($this->vars["meta"]) ? $this->vars["meta"] : false;
 $excerpt   = isset($this->vars["excerpt"]) ? $this->vars["excerpt"] : ($post ? __gulp_init_namespace___get_the_excerpt($post->ID, ["truncate" => ["count" => 55], "suffix" => ["value" => "&hellip;", "optional" => true]]) : false);
 ?>
 
 <?php if ($title || $permalink): ?>
-    <article class="article article--excerpt<?php echo $class; ?>">
+    <article class="article article--post-excerpt<?php echo $class; ?>">
 
         <?php if ($title || $meta): ?>
             <header class="article__header">
@@ -41,16 +41,14 @@ $excerpt   = isset($this->vars["excerpt"]) ? $this->vars["excerpt"] : ($post ? _
 
         <?php if ($excerpt): ?>
             <div class="article__content">
-                <div class="article__user-content user-content">
-                    <p class="article__text text<?php if ($light): ?> __light<?php endif; ?>">
-                        <?php echo $excerpt; ?>
-                        <?php if ($permalink): ?>
-                            <a class="text__link link<?php if ($light): ?> link--inherit<?php endif; ?>" href="<?php echo esc_url($permalink); ?>">
-                                <?php _e("Read More", "__gulp_init_namespace__"); ?>
-                            </a>
-                        <?php endif; ?>
-                    </p>
-                </div>
+                <p class="article__text text<?php if ($light): ?> __light<?php endif; ?>">
+                    <?php echo $excerpt; ?>
+                    <?php if ($permalink): ?>
+                        <a class="text__link link<?php if ($light): ?> link--inherit<?php endif; ?>" href="<?php echo esc_url($permalink); ?>">
+                            <?php _e("Read More", "__gulp_init_namespace__"); ?>
+                        </a>
+                    <?php endif; ?>
+                </p>
             </div><!--/.article__content-->
         <?php endif; ?>
 
